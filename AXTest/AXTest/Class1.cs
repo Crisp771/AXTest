44c2c8beedd25538c4a94a8519e99a52a87bca95
CWilson <cwilson@accidentexchange.com>
2017-10-06 10:45:55 +0100

    Initial Commit

---
+++ AXTest/AXTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXTest
+{
+    public class AXTest
+    {
+        public string HelloWorld()
+        {
+            return "Hello, World!";
+        }
+
+    }
+}
