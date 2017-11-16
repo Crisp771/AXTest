fd1af232888992f66d7947da74b93fb6462eb4ac
Cris Wilson <cris.wilson77@gmail.com>
2017-11-16 16:44:05 +0000

    Unit Tests Added

---
+++ AXTest/AXTestUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using AXTest;
+using NUnit.Framework;
+
+namespace AXTestUnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void HelloWorldTest()
+        {
+            var Target = new AXTest.AXTest();
+            var Expected = "Hello, World!";
+            var Actual = Target.HelloWorld();
+
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
